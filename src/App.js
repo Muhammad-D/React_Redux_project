2033e03739670fbf2e3bb30b9c6671238dae47c5
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-14 16:54:11 +0300

    LESSON 11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./App.css";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
-import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
-      <Footer />
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://cdn.pixabay.com/photo/2017/01/27/14/25/coffee-2013192_960_720.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="#h">Profile</a>
+        </div>
+        <div>
+          <a href="#h">Messages</a>
+        </div>
+        <div>
+          <a href="#h">News</a>
+        </div>
+        <div>
+          <a href="#h">Music</a>
+        </div>
+        <div>
+          <a href="#h">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2018/08/03/14/12/hummingbird-3581989_960_720.jpg" />
+          <div>ava + description</div>
+          <div>
+            My post
+            <div>New post </div>
+            <div>
+              <div>post 1</div>
+              <div>post 2</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
