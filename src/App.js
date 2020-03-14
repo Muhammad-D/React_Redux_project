c64bb05d781916807a2dbfff45ea22ecfc7beeeb
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-14 14:02:57 +0300

    LESSON 10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Technologies from "./Technologies";
+import Footer from "./Footer";
 import Header from "./Header";
 
 const App = () => {
   return (
     <div>
+      <Footer />
       <Header />
       <Technologies />
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="App">
+      <a href="#s">Home</a>
+      <br />
+      <a href="#s">News Feed</a>
+      <br />
+      <a href="#s">Messages</a>
+    </div>
+  );
+};
+
+export default Footer;
