6ff658a739d7e6a3a2eba3fd0ecba164370b465e
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-14 13:33:52 +0300

    LESSON 09

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-};
-
-const Technologies = () => {
-  return (
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JS</li>
-    </ul>
-  );
-};
-
-const Header = () => {
-  return (
-    <div className="App">
-      <a href="#s">Home</a>
-      <br />
-      <a href="#s">News Feed</a>
-      <br />
-      <a href="#s">Messages</a>
     </div>
   );
 };
