04f06387235c5a5608f3542a75291a096fb31e0f
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-22 16:59:37 +0300

    LESSON 16

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = props => {
   return (
     <div>
       <div className={s.item}>
         <img src="https://media.mnn.com/assets/images/2019/01/grumpy_cat.jpg.653x0_q80_crop-smart.jpg" />
-        post 1
+        {props.message}
         <div>
-          <span>Like</span> <span>DisLike</span>
+          <span> {props.likes} Like</span>
         </div>
       </div>
     </div>
