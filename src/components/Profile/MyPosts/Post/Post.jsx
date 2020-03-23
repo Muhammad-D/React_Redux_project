d266ba3fe14fa529b794212bd19563a91c880e31
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-23 08:49:29 +0300

    LESSON 18

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ const Post = props => {
   return (
     <div>
       <div className={s.item}>
-        <img src="https://media.mnn.com/assets/images/2019/01/grumpy_cat.jpg.653x0_q80_crop-smart.jpg" />
+        <img src="https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg" />
         {props.message}
         <div>
           <span> {props.likes} Like</span>
