905ce42b13dc0070a353d1adc303184f366f04f0
Muhammad Dzhabrailov <MuhammadDzhabrailov@MacBook-Air-Muhammad.local>
2020-03-23 12:04:00 +0300

    LESSON 19

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,27 +5,27 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div>
-        <a className={s.item} href="#h">
+        <a className={s.item} href="/profile">
           Profile
         </a>
       </div>
       <div>
-        <a className={`${s.item} ${s.active}`} href="#h">
+        <a className={`${s.item} ${s.active}`} href="/dialogs">
           Messages
         </a>
       </div>
       <div>
-        <a className={s.item} href="#h">
+        <a className={s.item} href="/news">
           News
         </a>
       </div>
       <div>
-        <a className={s.item} href="#h">
+        <a className={s.item} href="/music">
           Music
         </a>
       </div>
       <div>
-        <a className={s.item} href="#h">
+        <a className={s.item} href="/settings">
           Settings
         </a>
       </div>
