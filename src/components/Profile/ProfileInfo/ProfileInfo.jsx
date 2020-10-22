034a0a59ac8e29794f0d58b79f4c776bbc30ae93
MD <dzhabrailov.magomed3@gmail.com>
2020-10-22 10:58:44 +0300

    localState 71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileChange from "./ProfileChange/ProfileChange";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -8,15 +9,17 @@ const ProfileInfo = (props) => {
   }
   return (
     <div className={s.content}>
-      <div>
+      {/* <div>
         <img src="https://cdn.pixabay.com/photo/2018/08/03/14/12/hummingbird-3581989_960_720.jpg" />
-      </div>
+      </div> */}
       <div>
         <div>
           <img src={props.profile.photos.large} />
         </div>
-        ava + description
-      </div>
+        <div>
+          <ProfileChange status='See you there'/>
+        </div>   
+   </div>
     </div>
   );
 };
