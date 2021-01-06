6df3fbd39b17b9f312c14cf61ffae73215cd060d
MD <dzhabrailov.magomed3@gmail.com>
2021-01-06 22:00:55 +0300

    layout of login page estableshed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,33 +9,54 @@ import {
 } from "../common/FormsControlers/FormsControlers";
 import { logIn } from "../../redux/auth-reducer";
 import { Redirect } from "react-router-dom";
+
 import style from "./../common/FormsControlers/FormsControlers.module.css";
+import "./Login.scss";
+
+import logo from "../../assets/images/logo-icon.svg";
+
+import PersonIcon from "@material-ui/icons/Person";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <FieldCreater
-          placeholder="Login"
-          name="email"
-          component={InputArea}
-          validate={[requiredField]}
-          props={{ type: "text" }}
-        />
-        <FieldCreater
-          placeholder="Password"
-          name="password"
-          component={InputArea}
-          validate={[requiredField]}
-          props={{ type: "password" }}
-        />
-        <FieldCreater
-          name="rememberMe"
-          component={InputArea}
-          type="checkbox"
-          props={{ type: "checkbox" }}
-          text="remember me"
-        />
+      <form className="login__form" onSubmit={handleSubmit}>
+        <div className="login__input-wrapper">
+          <span className="login__icon">
+            <PersonIcon />
+          </span>
+          <FieldCreater
+            placeholder="Login"
+            name="email"
+            component={InputArea}
+            validate={[requiredField]}
+            props={{ type: "text" }}
+          />
+        </div>
+        <div className="login__input-wrapper">
+          <span className="login__icon">
+            <LockOpenIcon />
+          </span>
+          <FieldCreater
+            placeholder="Password"
+            name="password"
+            component={InputArea}
+            validate={[requiredField]}
+            props={{ type: "password" }}
+          />
+        </div>
+        <label className="login__checkbox-label">
+          {" "}
+          <FieldCreater
+            name="rememberMe"
+            component={InputArea}
+            type="checkbox"
+            props={{ type: "checkbox" }}
+          />{" "}
+          <span>Remember</span>
+        </label>
+
         {captchaUrl && <img src={captchaUrl} />}
         {captchaUrl && (
           <FieldCreater
@@ -66,13 +87,17 @@ class Login extends React.Component {
     return this.props.isAuth ? (
       <Redirect to="/profile" />
     ) : (
-      <>
-        <h1>LOGIN</h1>
-        <LoginReduxForm
-          onSubmit={this.reduxOnSubmit}
-          captchaUrl={this.props.captchaUrl}
-        />
-      </>
+      <div className="login">
+        <section className="login__wrapper">
+          <img src={logo} alt="logo" className="login__logo" />
+          <h1 className="login__title">Welcome</h1>
+          <p className="login__text">Join us us today</p>
+          <LoginReduxForm
+            onSubmit={this.reduxOnSubmit}
+            captchaUrl={this.props.captchaUrl}
+          />
+        </section>
+      </div>
     );
   }
 }
