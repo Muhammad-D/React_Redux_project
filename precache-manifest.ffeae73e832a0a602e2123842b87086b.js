ed425f12c6fbac71b0f513851f213fc2dc7d1af5
MD <dzhabrailov.magomed3@gmail.com>
2021-01-14 08:50:19 +0300

    Updates

---
+++ precache-manifest.ffeae73e832a0a602e2123842b87086b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee39a8274b28e557d72f60ccdaa685b0",
+    "url": "/React_Redux_project/index.html"
+  },
+  {
+    "revision": "362a57a02f8798a3bdd7",
+    "url": "/React_Redux_project/static/css/3.3657edf7.chunk.css"
+  },
+  {
+    "revision": "e7c58fb363ee886edab9",
+    "url": "/React_Redux_project/static/css/4.6815f673.chunk.css"
+  },
+  {
+    "revision": "74cdd0d34e94092fdcd8",
+    "url": "/React_Redux_project/static/css/5.824bdbeb.chunk.css"
+  },
+  {
+    "revision": "451dd61d9f7ff253068b",
+    "url": "/React_Redux_project/static/css/main.6ab783f7.chunk.css"
+  },
+  {
+    "revision": "83dbcebf11d49dda2113",
+    "url": "/React_Redux_project/static/js/2.0c5ec586.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/React_Redux_project/static/js/2.0c5ec586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362a57a02f8798a3bdd7",
+    "url": "/React_Redux_project/static/js/3.46d36f9d.chunk.js"
+  },
+  {
+    "revision": "e7c58fb363ee886edab9",
+    "url": "/React_Redux_project/static/js/4.60b5d4db.chunk.js"
+  },
+  {
+    "revision": "74cdd0d34e94092fdcd8",
+    "url": "/React_Redux_project/static/js/5.dae73ca6.chunk.js"
+  },
+  {
+    "revision": "451dd61d9f7ff253068b",
+    "url": "/React_Redux_project/static/js/main.de79fc1b.chunk.js"
+  },
+  {
+    "revision": "55b5bef0a228f931e870",
+    "url": "/React_Redux_project/static/js/runtime-main.9a828ad7.js"
+  },
+  {
+    "revision": "811d995377134eb5eb217d41682e8a14",
+    "url": "/React_Redux_project/static/media/background-image.811d9953.jpg"
+  },
+  {
+    "revision": "faa1770df438c8adb7129ce627b56090",
+    "url": "/React_Redux_project/static/media/logo-icon.faa1770d.svg"
+  },
+  {
+    "revision": "3570cafa01c6b8faa4cda592b65b4c5d",
+    "url": "/React_Redux_project/static/media/tenor.3570cafa.gif"
+  },
+  {
+    "revision": "8e559fa799970928caba19fbd0e250f6",
+    "url": "/React_Redux_project/static/media/users.8e559fa7.webp"
+  }
+]);
